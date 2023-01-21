c55a03b293d5eacd489e0b23797c62999f54a2a9
Sam B <reposme@outlook.com>
2023-01-21 22:39:00 +0000

    Swaped out the checkbox questions for list questions and added a comprehensive list of license types

--- starter/index.js
+++ starter/index.js
@@ -8,6 +8,17 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // array of questions for user
 const questions = [
     {
+        // When a user enters their GitHub username then this is added to the section of the README entitled Questions, with a link to their GitHub profile
+        type: 'input',
+        name: 'github_username',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email_address',
+        message: 'What is your email address?',
+    },
+    {
         type: 'input',
         name: 'project_title',
         message: 'What is the project title?',
@@ -41,85 +52,69 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'questions',
-        message: 'Where should users direct their questions?',
-    },
-
-    {
-        type: 'input',
         name: 'deployed_site_url',
         message: 'Provide the URL for your deployed site',
     },
-        {
-        type: 'checkbox',
-        message: 'Select license',
+    {
+        type: 'list',
         name: 'license',
+        message: 'Select license',
         choices: [
-            {
-                name: 'MIT',
-            },
-            {
-                name: 'Other',
-            },
-            {
-                name: 'GPLv2',
-            },
-            {
-                name: 'Apache',
-            },]
-    }
+            'MIT',
+            'Apache License 2.0',
+            'Academic Free License v3.0',
+            'Artistic license 2.0',
+            'Boost Software License 1.0',
+            'BSD 2-clause "Simplified" license',
+            'BSD 3-clause "New" or "Revised" license',
+            'BSD 3-clause Clear license',
+            'Creative Commons license family',
+            'Creative Commons Zero v1.0 Universal',
+            'Creative Commons Attribution 4.0',
+            'Creative Commons Attribution Share Alike 4.0',
+            'Do What The F*ck You Want To Public License',
+            'Educational Community License v2.0',
+            'Eclipse Public License 1.0',
+            'Eclipse Public License 2.0',
+            'European Union Public License 1.1',
+            'GNU Affero General Public License v3.0',
+            'GNU General Public License family',
+            'GNU General Public License v2.0',
+            'GNU General Public License v3.0',
+            'GNU Lesser General Public License family',
+            'GNU Lesser General Public License v2.1',
+            'GNU Lesser General Public License v3.0',
+            'ISC',
+            'LaTeX Project Public License v1.3c',
+            'Microsoft Public License',
+            'Mozilla Public License 2.0',
+            'Open Software License 3.0',
+            'PostgreSQL License',
+            'SIL Open Font License 1.1',
+            'University of Illinois/NCSA Open Source License',
+            'The Unlicense',
+            'zLib License',
+
+
+        ],
+    },
+    {
+        type: 'input',
+        name: 'insert_license_text',
+        message: 'Please insert license text here',
+    },
 
 ];
 
 
-// // array for the table of contents
-// const tableOfContents = ["Table of Contents", "Installation", "Usage", "License", "Contributing", "Tests", "Questions"];
-// // console.log(tableOfContents);
-
-// inquirer.prompt([
-//     {
-//         type: 'checkbox',
-//         message: 'Select license',
-//         name: 'license',
-//         choices: [
-//             {
-//                 name: 'MIT',
-//             },
-//             {
-//                 name: 'Other',
-//             },
-//             {
-//                 name: 'GPLv2',
-//             },
-//             {
-//                 name: 'Apache',
-//             },]
-//     },
-// ])
-//     .then((answers) => {
-//         console.log(JSON.stringify(answers, null, '  '));
-//     });
-
-
-
-
-
-
 // When a user chooses a license for their application from a list of options then a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-
-// When a user enters their GitHub username then this is added to the section of the README entitled Questions, with a link to their GitHub profile
-
-// When a user enters their email address then this is added to the section of the README entitled Questions, with instructions on how to reach them with additional questions
-
-// When a user clicks on the links in the Table of Contents then they are taken to the corresponding section of the README
-
 
 
 // function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data,
         (err) => err ? console.log(err) : console.log('Success!'));
-        console.log(data.title);
+    console.log(data.title);
 }
 
 // function to initialize program
@@ -131,7 +126,7 @@ function init() {
             console.log(response);
         }
         );
-    }
+}
 
 
 // function call to initialize program
