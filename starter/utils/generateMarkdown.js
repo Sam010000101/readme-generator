3cba63bbe10569f1ae62c14467f24f2d3d5923c6
Sam B <reposme@outlook.com>
2023-01-21 21:33:14 +0000

    Updated ToC and contacts in Questions section

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,16 +1,17 @@
 // function to generate markdown for README
+// When a user clicks on the links in the Table of Contents then they are taken to the corresponding section of the README
 function generateMarkdown(data) {
   return `# ${data.project_title}
   ## Description
   ${data.project_description}
  ## Table of Contents
- ### Installation
- ### Usage
- ### Tests
- ### Contributing
- ### Questions
- ### Deployed Site URL
- ### License
+ * [Installation](#installation) 
+ * [Usage](#usage) 
+ * [Tests](#tests) 
+ * [Contributing](#contributing) 
+ * [Questions](#questions) 
+ * [Deployed Site URL](#deployed-site-url) 
+ * [License](#license) 
 
   ## Installation
   ${data.installation_instructions}
@@ -21,8 +22,9 @@ function generateMarkdown(data) {
   ## Contributing
   ${data.contributing}
   ## Questions
-  You can direct any questions that you may have regarding this project to;
-  ${'https://github.com/' + data.github_username}
+  You can direct any questions that you may have regarding this project to; \n
+  ${'* ' + 'GitHub: ' + '[' + data.github_username + ']' + '(https://github.com/' + data.github_username +')'} \n
+  ${'* ' + 'Email: ' + data.email_address}
   ## Deployed Site URL
   ${data.deployed_site_url}
   ## License
