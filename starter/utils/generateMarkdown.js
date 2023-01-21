bd6625a57c9a27e1535daece8867346a1817770d
Sam B <reposme@outlook.com>
2023-01-21 20:31:17 +0000

    Included generic github url as a prefix that concatenates with the input of the username so that it creates a url link to the user profile when the user adds their github username. Also added some standard text to the Questions section

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -3,6 +3,15 @@ function generateMarkdown(data) {
   return `# ${data.project_title}
   ## Description
   ${data.project_description}
+ ## Table of Contents
+ ### Installation
+ ### Usage
+ ### Tests
+ ### Contributing
+ ### Questions
+ ### Deployed Site URL
+ ### License
+
   ## Installation
   ${data.installation_instructions}
   ## Usage
@@ -12,7 +21,8 @@ function generateMarkdown(data) {
   ## Contributing
   ${data.contributing}
   ## Questions
-  ${data.questions}
+  You can direct any questions that you may have regarding this project to;
+  ${'https://github.com/' + data.github_username}
   ## Deployed Site URL
   ${data.deployed_site_url}
   ## License
