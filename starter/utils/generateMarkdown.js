cd4bcd678ec36e82b566f3085e2db928f03afd91
Sam B <reposme@outlook.com>
2023-01-21 19:27:26 +0000

    Inclusion of markdown for generating answers that reference the const questions index.js

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,8 +1,33 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.project_title}
+          ## ${data.project_description}
+          ## ${data.installation_instructions}
+          ## ${data.usage_information}
+          ## ${data.tests}
+          ## ${data.contributing}
+          ## ${data.questions}
+          ## ${data.deployed_site_url}
+
+# Yo
+## Yo
+## Yo
+**Yo**
+*Yo*
+> Yo
+1. Yo
+2. Yo
+3. Yo
+- Yo
+- Yo
+- Yo
+---
+[title](https://www.yo.com)
+![alt text](yo.jpg)
 
 `;
 }
 
 module.exports = generateMarkdown;
+
+
