c46101b8e67e5b9cdf72e6aab89719609c99941a
Sam B <reposme@outlook.com>
2023-01-24 07:57:10 +0000

    Inclusion of a prompt to select the license badge for the chosen license

--- starter/index.js
+++ starter/index.js
@@ -94,9 +94,50 @@ const questions = [
             'University of Illinois/NCSA Open Source License',
             'The Unlicense',
             'zLib License',
+        ]
 
-
+    },
+    {
+        type: 'list',
+        name: 'badges',
+        message: 'Select license badge',
+        choices: [
+            '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+            'Apache License 2.0',
+            'Academic Free License v3.0',
+            'Artistic license 2.0',
+            'Boost Software License 1.0',
+            'BSD 2-clause "Simplified" license',
+            'BSD 3-clause "New" or "Revised" license',
+            'BSD 3-clause Clear license',
+            'Creative Commons license family',
+            'Creative Commons Zero v1.0 Universal',
+            'Creative Commons Attribution 4.0',
+            'Creative Commons Attribution Share Alike 4.0',
+            'Do What The F*ck You Want To Public License',
+            'Educational Community License v2.0',
+            'Eclipse Public License 1.0',
+            'Eclipse Public License 2.0',
+            'European Union Public License 1.1',
+            'GNU Affero General Public License v3.0',
+            'GNU General Public License family',
+            'GNU General Public License v2.0',
+            'GNU General Public License v3.0',
+            'GNU Lesser General Public License family',
+            'GNU Lesser General Public License v2.1',
+            '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+            'ISC',
+            'LaTeX Project Public License v1.3c',
+            'Microsoft Public License',
+            'Mozilla Public License 2.0',
+            'Open Software License 3.0',
+            'PostgreSQL License',
+            'SIL Open Font License 1.1',
+            'University of Illinois/NCSA Open Source License',
+            'The Unlicense',
+            'zLib License',
         ],
+
     },
     {
         type: 'input',
@@ -112,20 +153,20 @@ const questions = [
 // // https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
 
 
-const licenseBadges = [
-    {
-        license: 'MIT',
-        badge: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-    },
-    {
-        license: 'GPL v3',
-        badge: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
-    }
-];
+// const licenseBadges = [
+//     {
+//         license: 'MIT',
+//         badge: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+//     },
+//     {
+//         license: 'GPL v3',
+//         badge: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+//     }
+// ];
 
 // if (questions.license === 'MIT')
 // console.log('MIT');
-console.log(questions[9].choices);
+// console.log(questions[9].choices);
 
 // .then((response) =>
 // {if (licenseB.badge == "MIT") 
@@ -170,6 +211,6 @@ function init() {
         );
 }
 
-
 // function call to initialize program
 init();
+
